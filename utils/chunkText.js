9d67103440c390d64c04e81c28f93fc47b26c5d7
andrewbloese-00 <dev@andrewbloese.me>
2023-07-19 15:10:56 -0700

    optimal chunksize function for more even chunks when over rate limits

--- utils/chunkText.js
+++ utils/chunkText.js
@@ -29,6 +29,80 @@ function chunkText(text) {
       chunks.push(currentChunk);
     }
     return chunks;
+}
+
+
+const countTokens = text => Math.ceil(text.length/4)
+
+function chunkTextV2(text,tokensPerChunk=300){
+  //calculate number of tokens in input
+  const textTokensEstimate = countTokens(text)
+
+  if(textTokensEstimate < tokensPerChunk){
+    return [text]
   }
 
-module.exports = {chunkText}
+
+
+  //get sentences from input
+  const sentences = text.split(".")
+  let chunks = []
+  let currentChunk = ""
+  for(let s = 0; s < sentences.length; s++){
+    let sentenceTokens = countTokens(sentences[s])
+    if(countTokens(currentChunk) + sentenceTokens > tokensPerChunk){
+      chunks.push(currentChunk)
+      currentChunk = ""
+    }
+    currentChunk += sentences[s] + ". "
+  }
+
+  if(currentChunk.length > 0){
+    chunks.push(currentChunk)
+  }
+  return chunks
+
+}
+
+
+function chunkIntoOptimalEvenGroups(inputString) {
+  const length = inputString.length;
+  const minChunkSize = 1000; // Minimum number of tokens per chunk
+  const maxChunkSize = 4000; // Maximum number of tokens per chunk
+
+
+  if(countTokens(inputString) < TOKEN_LIMIT){
+    return [inputString]
+  }
+
+
+  let chunkSize = Math.ceil(length / 2);
+  let numChunks = 2;
+
+  //determine optimal even chunk size
+  for (let i = 2; i <= Math.ceil(length / minChunkSize); i++) {
+    if (length % i === 0) {
+      const currentChunkSize = length / i;
+      if (
+        currentChunkSize >= minChunkSize &&
+        currentChunkSize <= maxChunkSize 
+      ) {
+        if (
+         ( currentChunkSize < chunkSize ||
+          (currentChunkSize === chunkSize && i < numChunks)) 
+        ) {
+          chunkSize = currentChunkSize;
+          numChunks = i;
+        }
+      }
+    }
+  }
+
+  //use chunking helper function to get chunks of optimal size
+  const chunks = chunkTextV2(inputString,chunkSize)
+  
+  return chunks;
+  
+}
+
+module.exports = {chunkIntoOptimalEvenGroups}
--- utils/gptHelpers.js
+++ utils/gptHelpers.js
@@ -1,28 +1,28 @@
 const {Configuration,OpenAIApi} = require("openai")
-const { chunkText } = require("./chunkText")
+const { chunkText, chunkIntoOptimalEvenGroups } = require("./chunkText")
 const apiKey = require("../config.json").OPENAI_API_KEY
 
 const config = new Configuration({apiKey})
 const openai = new OpenAIApi(config)
 const SYS_MSG = "You are an assistant who specializes in creating useful summaries of raw text content from the web. You are only to consider the relevant information on a webpage, disregarding navbars, buttons and other html elements. Also ignore state information such as 'cart is empty' "
 const OPENAI_MODEL = "gpt-3.5-turbo"
-
+const MAX_DEPTH = 4
 /**
  * 
  * @param {string} input the input to be summarized 
  * @param {number} temperature *optional* the temparature to pass to the gpt model
  * @returns  {Promise<{error?: any, summary?:string}>}
  */
-async function summaryCompletion(input,temperature=0.8,format="tl;dr like summary in the form of a paragraph"){
+async function summaryCompletion(input,temperature=0.1,format="tl;dr like summary writen like a paragraph (or more)"){
     try {
         const messages = [ 
             { role: "system", content: SYS_MSG },
-            { role: "user", content: `Summarize the following text in a consise manner. It should formatted like: ${format}. Text = ${input}`}
+            { role: "user", content: `Write a summary of the 'Text', it should formatted like: ${format}. Text = ${input}`}
         ]
         const response = await openai.createChatCompletion({
             model: OPENAI_MODEL,
             temperature,
-            messages
+            messages,
         })
         
         const summary = response.data.choices[0].message.content;
@@ -39,16 +39,14 @@ async function summaryCompletion(input,temperature=0.8,format="tl;dr like summar
  * @about a recursive function that will generate new summaries for each section of the text until it is within the token limits of gpt-3.5.turbo
  * @returns { Promise<{error:any} | Promise<{summary:string}> }
  */
-async function summarize(text = "",temperature=0.1,format="tl;dr like summary in the form of a paragraph"){
-    //returns an array of chunks that are within token limits to prompt summaries with
-   const chunked = chunkText(text)
-   if(chunked.length === 1) { //base case 
+async function summarize(text = "",temperature=0.1,format="tl;dr like summary in the form of a paragraph",d=0){
+   const chunked = chunkIntoOptimalEvenGroups(text)
+   if(chunked.length === 1 || d > MAX_DEPTH) { 
         const {summary ,error }= await summaryCompletion(chunked[0],temperature,format)
         if(error) return { error}
         return {summary}
    }
-
-    //get summary for each chunk
+    console.log('OpenAI is typing...')
     const summaryQ = chunked.map( chunk => summaryCompletion(chunk,temperature,format))
     const summariesOfChunks = await Promise.all(summaryQ)
     
@@ -58,8 +56,13 @@ async function summarize(text = "",temperature=0.1,format="tl;dr like summary in
         .map(s=>s.summary)
         .join("\n")
     
-    return summarize(summaryText,temperature,format)
+    return summarize(summaryText,temperature,format,d+1)
 }
+
+
+
+
+
 
 
 
