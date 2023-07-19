befefdaaa8bd49d8c6f40fd6686597b8561d3018
andrewbloese-00 <dev@andrewbloese.me>
2023-07-18 21:36:50 -0700

    Implemented chunking function to ensure token limit is not exceeded.

---
+++ utils/chunkText.js
@@ -0,0 +1,33 @@
+//note that each token is roughly 4 characters 
+const TOKEN_LIMIT = 4000;
+
+function chunkText(text) {
+
+    // Check if text is within the token limit
+    if ( (text.length/4) <= TOKEN_LIMIT) {
+      return [text];
+    }
+  
+    const sentences = text.split('.'); 
+    const chunks = [];
+  
+    let currentChunk = '';
+    for (let i = 0; i < sentences.length; i++) {
+      const sentence = sentences[i];
+      const tempChunk = currentChunk + sentence + '.';
+      
+      if ((tempChunk.length/4) > TOKEN_LIMIT) {
+        chunks.push(currentChunk);
+        currentChunk = ''; 
+      }
+  
+      currentChunk += sentence + '.';
+    }
+    //ensure all chunks are done
+    if (currentChunk.length > 0) {
+      chunks.push(currentChunk);
+    }
+    return chunks;
+  }
+
+module.exports = {chunkText}
